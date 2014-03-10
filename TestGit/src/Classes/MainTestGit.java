b7c52981157727396befb7d96f997575d48d990a
Annie Pourriere <gitannie@annie.pourriere@gmail.com>
2014-03-10 11:44:29 +0100

    Modif de MainTestGit

--- TestGit/src/Classes/MainTestGit.java
+++ TestGit/src/Classes/MainTestGit.java
@@ -4,7 +4,9 @@ public class MainTestGit {
 	
 	    public static void main (String[] args)
 	    {
-	        // Modif Annie 10/03/14
+	        //Ceci est mon nouveau commentaire
+	    	// pour l'exercice version2
+	    	// Modif Annie 10/03/14
 	    	Atelier monAtelier;
 	    	monAtelier= new Atelier(1,1,"info",10);
 	    	System.out.print ("Voici mon premier atelier : ");
